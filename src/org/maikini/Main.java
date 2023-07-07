12493f3a65764cb3e684bd61f7abab2a12527b9e
Christopher Pooya Razavian <pooya72@gmail.com>
2023-07-07 16:08:12 +0100

    Add logging

--- src/org/maikini/Main.java
+++ src/org/maikini/Main.java
@@ -4,12 +4,15 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.logging.Logger;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+        final var logger = Logger.getLogger("NoteLogger");
         final var socketAddress = new InetSocketAddress("localhost", 8000);
         final var server = HttpServer.create(socketAddress, 0);
         server.start();
-        System.out.println("Hello world");
+        logger.info("Server is up and running at: http:/" + server.getAddress());
     }
 }
