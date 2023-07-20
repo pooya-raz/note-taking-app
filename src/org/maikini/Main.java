9467a186d5f5b67b9e71dd9ba49718e6388966d9
Christopher Pooya Razavian <pooya72@gmail.com>
2023-07-20 14:26:34 +0100

    Setup sample HttpHandler

--- src/org/maikini/Main.java
+++ src/org/maikini/Main.java
@@ -1,5 +1,7 @@
 package org.maikini;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
@@ -10,11 +12,21 @@ import static java.lang.System.Logger.Level.INFO;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        final var log = System.getLogger("NoteLogger");
-
+        final var logger = System.getLogger("NoteLogger");
         final var socketAddress = new InetSocketAddress("localhost", 8000);
         final var server = HttpServer.create(socketAddress, 0);
         server.start();
-        log.log(INFO, "Server is up and running at: http:/" + server.getAddress());
+        final var bob = new Handler();
+        server.createContext("/", bob);
+        logger.log(INFO, "Server is up and running at: http:/" + server.getAddress());
+    }
+}
+
+class Handler implements HttpHandler {
+    System.Logger logger = System.getLogger(Handler.class.getName());
+
+    public void handle(HttpExchange exchange) {
+        logger.log(INFO, "The request method is: " + exchange.getRequestMethod());
+        logger.log(INFO, "Bob is handling the exchange");
     }
 }
