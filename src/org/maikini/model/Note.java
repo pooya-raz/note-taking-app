cd8fad21c8ea3b1a00b90238afca7d2a842dd0f6
Christopher Pooya Razavian <pooya72@gmail.com>
2023-08-18 14:50:06 +0100

    Create Note model

---
+++ src/org/maikini/model/Note.java
@@ -0,0 +1,3 @@
+package org.maikini.model;
+
+public record Note(String title, String body) {}
--- src/org/maikini/service/NoteService.java
+++ src/org/maikini/service/NoteService.java
@@ -1,4 +1,6 @@
-package org.maikini;
+package org.maikini.service;
+
+import org.maikini.model.Note;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,18 +14,5 @@ public class NoteService {
     // need a way to add a new note to the list [ method ]
     public void addNote(String title, String body) {
         //        😓 append? how do I add a note to the notes List<>? It should take a note and add it. TODO
-    }
-
-    // nested class to represent a single note
-    static class Note {
-        private final String title; // variable to store title
-        private final String body; // variable to store title
-
-        // need a way to build/manufacture a note [constructor]
-
-        public Note(String title, String body) {
-            this.title = title;
-            this.body = body;
-        }
     }
 }
